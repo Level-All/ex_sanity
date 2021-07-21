4ffc6b41c317325d57f280a82981ae4f4dc20da7
Sebastian Odell <sebastian@sanctuary.computer>
2021-07-21 08:40:14 -0400

    updates config

--- lib/ex_sanity.ex
+++ lib/ex_sanity.ex
@@ -3,7 +3,7 @@ defmodule ExSanity do
     %{
       file_base: Application.get_env(:ex_sanity, :file_base),
       project_id: Application.get_env(:ex_sanity, :project_id, "xxxx"),
-      dataset: Application.get_env(:ex_sanity, :project_id, "production"),
+      dataset: Application.get_env(:ex_sanity, :dataset, "production"),
       api_key: Application.get_env(:ex_sanity, :api_key, "xxxx"),
       version: Application.get_env(:ex_sanity, :version, "v2021-06-07"),
       endpoint: Application.get_env(:ex_sanity, :endpoint, "api")
