1aa3278d6f4f3d6c97d3698c46b172e2d09d8827
Sebastian Odell <sebastian@sanctuary.computer>
2021-09-14 14:00:33 -0400

    adds custom serializer support

--- lib/ex_sanity/portable_text.ex
+++ lib/ex_sanity/portable_text.ex
@@ -3,8 +3,11 @@ defmodule ExSanity.PortableText do
 
   alias ExSanity.PortableText.{Serializers, Lists}
 
-  def to_html(nodes \\ []) do
-    render_container(nodes |> Lists.with_list_blocks() |> Enum.map(fn node -> render_node(node) end))
+  def to_html(nodes, custom_serializers \\ %{}) do
+    base_serializers = Serializers.serializers()
+    serializers = merge_serializers(base_serializers, custom_serializers)
+
+    render_container(nodes |> Lists.with_list_blocks() |> Enum.map(fn node -> render_node(serializers, node) end))
   end
 
   def render_container(children) do
@@ -12,20 +15,35 @@ defmodule ExSanity.PortableText do
   end
 
   def render_node(
+    serializers,
      node = %{
        "markDefs" => mark_defs
      }
-   ), do: match_node(node, mark_defs)
+   ), do: match_node(serializers, node, mark_defs)
 
-  def render_node(node, mark_defs \\ []), do: match_node(node, mark_defs)
+  def render_node(serializers, node, mark_defs \\ []), do: match_node(serializers, node, mark_defs)
 
-  defp match_node(node, mark_defs) do
+  defp match_node(serializers, node, mark_defs) do
     case node do
-      %{"_type" => "list"} -> Serializers.get_serializer([:list]).(node)
-      %{"_type" => "span"} -> Serializers.get_serializer([:span]).(node, mark_defs)
-      %{"_type" => "block", "children" => _, "listItem" => _} -> Serializers.get_serializer([:list_item]).(node)
-      %{"_type" => "block"} -> Serializers.get_serializer([:block]).(node)
-      %{"_type" => type} -> Serializers.get_serializer([String.to_atom(type)]).(node)
+      %{"_type" => "list"} -> serializers[:list].(serializers, node, mark_defs)
+      %{"_type" => "span"} -> serializers[:span].(serializers, node, mark_defs)
+      %{"_type" => "block", "children" => _, "listItem" => _} -> serializers[:list_item].(serializers, node, mark_defs)
+      %{"_type" => "block"} -> serializers[:block].(serializers, node, mark_defs)
+      %{"_type" => "image"} -> serializers[:image].(serializers, node, mark_defs)
+      %{"_type" => type} -> serializers[String.to_atom(type)].(serializers, node, mark_defs)
+    end
+  end
+
+  defp merge_serializers(base_serializers, custom_serializers) do
+    Map.merge(base_serializers, custom_serializers, &resolve_serializers/3)
+  end
+
+  defp resolve_serializers(key, serializer_val, custom_val) do
+    case key do
+      :marks ->
+        Map.merge(serializer_val, custom_val)
+      _ ->
+        custom_val
     end
   end
 end
