42537c7adb3f0ca65e516b711eafb82786b9a860
Conor Davidson <conor@conordavidson.com>
2021-11-11 18:11:31 -0800

    Progress

--- lib/ex_sanity/asset_builder.ex
+++ lib/ex_sanity/asset_builder.ex
@@ -3,53 +3,122 @@ defmodule ExSanity.AssetBuilder do
   def project_id, do: ExSanity.Config.resolve(:project_id)
   def dataset, do: ExSanity.Config.resolve(:dataset)
 
-  def image_url(image, opts \\ nil)
+  def image_url(%{
+        source: %{
+          "asset" => %{
+            "_ref" => ref
+          },
+          "crop" => %{
+            "bottom" => crop_bottom,
+            "left" => crop_left,
+            "right" => crop_right,
+            "top" => crop_top
+          },
+          "hotspot" => %{
+            "height" => hotspot_height,
+            "width" => hotspot_width,
+            "x" => hotspot_x,
+            "y" => hotspot_y
+          }
+        },
+        transforms: transforms
+      }) do
+    asset =
+      %{
+        id: _id,
+        width: width,
+        height: height,
+        format: _format
+      } = parse_image_asset(ref)
 
-  def image_url(image = %{"asset" => _asset}, opts) do
-    options = make_options(image, opts)
-    apply_options(image["asset"]["url"], options)
-  end
+    cleft = round(crop_left * width)
+    ctop = round(crop_top * height)
+    cwidth = round(width - crop_right * width - cleft)
+    cheight = round(height - crop_bottom * height - ctop)
 
-  def image_url(image_ref_or_url, opts) do
-    if String.contains?(image_ref_or_url, file_base()) do
-      image_ref_or_url
+    crop = %{
+      left: cleft,
+      top: ctop,
+      width: cwidth,
+      height: cheight
+    }
+
+    hotspot_vertical_radius = hotspot_height * height / 2
+    hotspot_horizontal_radius = hotspot_width * height / 2
+    hotspot_center_x = hotspot_x * width
+    hotspot_center_y = hotspot_y * height
+
+    hotspot = %{
+      left: hotspot_center_x - hotspot_horizontal_radius,
+      top: hotspot_center_y - hotspot_vertical_radius,
+      right: hotspot_center_x + hotspot_horizontal_radius,
+      bottom: hotspot_center_y + hotspot_vertical_radius
+    }
+
+    # If irrelevant, or if we are requested to: don't perform crop/fit based on
+    # the crop/hotspot.
+    if !(transforms[:rect] || transforms[:focal_point] || transforms[:ignore_image_params] ||
+           transforms[:crop]) do
+      spec_to_image_url(
+        Map.merge(
+          %{asset: asset},
+          fit(%{
+            crop: crop,
+            hotspot: hotspot,
+            transforms: transforms
+          })
+        )
+      )
     else
-      build_image_url(image_ref_or_url)
-    end
-    |> apply_options(opts)
-  end
-
-  def file_url(file_ref_or_url) do
-    if String.contains?(file_ref_or_url, file_base()) do
-      file_ref_or_url
-    else
-      build_file_url(file_ref_or_url)
+      spec_to_image_url(Map.merge(%{asset: asset}, transforms))
     end
   end
 
-  def build_image_url(image_ref) do
-    %{
-      id: id,
-      width: width,
-      height: height,
-      format: format
-    } = parse_image_asset(image_ref)
+  # def image_url(image = %{"asset" => _asset}, opts) do
+  #   options = make_options(image, opts)
+  #   apply_options(image["asset"]["url"], options)
+  # end
 
-    filename = "#{id}-#{width}x#{height}.#{format}"
+  # def image_url(image_ref_or_url, opts) do
+  #   if String.contains?(image_ref_or_url, file_base()) do
+  #     image_ref_or_url
+  #   else
+  #     build_image_url(image_ref_or_url)
+  #   end
+  #   |> apply_options(opts)
+  # end
 
-    "#{file_base()}/images/#{project_id()}/#{dataset()}/#{filename}"
-  end
+  # def file_url(file_ref_or_url) do
+  #   if String.contains?(file_ref_or_url, file_base()) do
+  #     file_ref_or_url
+  #   else
+  #     build_file_url(file_ref_or_url)
+  #   end
+  # end
 
-  def build_file_url(file_ref) do
-    %{
-      id: id,
-      format: format
-    } = parse_file_asset(file_ref)
+  # def build_image_url(image_ref) do
+  #   %{
+  #     id: id,
+  #     width: width,
+  #     height: height,
+  #     format: format
+  #   } = parse_image_asset(image_ref)
 
-    filename = "#{id}.#{format}"
+  #   filename = "#{id}-#{width}x#{height}.#{format}"
 
-    "#{file_base()}/files/#{project_id()}/#{dataset()}/#{filename}"
-  end
+  #   "#{file_base()}/images/#{project_id()}/#{dataset()}/#{filename}"
+  # end
+
+  # def build_file_url(file_ref) do
+  #   %{
+  #     id: id,
+  #     format: format
+  #   } = parse_file_asset(file_ref)
+
+  #   filename = "#{id}.#{format}"
+
+  #   "#{file_base()}/files/#{project_id()}/#{dataset()}/#{filename}"
+  # end
 
   defp parse_image_asset(image_ref) do
     [_type, id, width_x_height, format] = String.split(image_ref, "-")
@@ -58,42 +127,42 @@ defmodule ExSanity.AssetBuilder do
 
     %{
       id: id,
-      width: width,
-      height: height,
+      width: String.to_integer(width),
+      height: String.to_integer(height),
       format: format
     }
   end
 
-  defp parse_file_asset(file_ref) do
-    [_type, id, format] = String.split(file_ref, "-")
+  # defp parse_file_asset(file_ref) do
+  #   [_type, id, format] = String.split(file_ref, "-")
 
-    %{
-      id: id,
-      format: format
-    }
-  end
+  #   %{
+  #     id: id,
+  #     format: format
+  #   }
+  # end
 
-  @spec_name_to_url_name_mapping %{
-    width: "w",
-    height: "h",
-    format: "fm",
-    download: "dl",
-    blur: "blur",
-    sharpen: "sharpen",
-    invert: "invert",
-    orientation: "or",
-    min_height: "min-h",
-    max_height: "max-h",
-    min_width: "min-w",
-    max_width: "max-w",
-    quality: "q",
-    fit: "fit",
-    crop: "crop",
-    saturation: "sat",
-    auto: "auto",
-    dpr: "dpr",
-    pad: "pad"
-  }
+  @spec_name_to_url_name_mapping [
+    {:width, "w"},
+    {:height, "h"},
+    {:format, "fm"},
+    {:download, "dl"},
+    {:blur, "blur"},
+    {:sharpen, "sharpen"},
+    {:invert, "invert"},
+    {:orientation, "or"},
+    {:min_height, "min-h"},
+    {:max_height, "max-h"},
+    {:min_width, "min-w"},
+    {:max_width, "max-w"},
+    {:quality, "q"},
+    {:fit, "fit"},
+    {:crop, "crop"},
+    {:saturation, "sat"},
+    {:auto, "auto"},
+    {:dpr, "dpr"},
+    {:pad, "pad"}
+  ]
 
   def make_options(_image, opts) do
     # TODO: Handle crops and hotspots like:
@@ -114,5 +183,176 @@ defmodule ExSanity.AssetBuilder do
       end
     end)
     |> URI.encode_query()
+  end
+
+  def fit(%{
+        crop:
+          crop = %{
+            left: crop_left,
+            top: crop_top,
+            width: crop_width,
+            height: crop_height
+          },
+        hotspot: %{
+          left: hotspot_left,
+          top: hotspot_top,
+          right: hotspot_right,
+          bottom: hotspot_bottom
+        },
+        transforms: transforms
+      }) do
+    img_width = transforms[:width]
+    img_height = transforms[:height]
+
+    if !(img_width && img_height) do
+      %{
+        width: img_width,
+        height: img_height,
+        rect: crop
+      }
+    else
+      # If we are here, that means aspect ratio is locked and fitting will be a bit harder
+      desired_aspect_ratio = img_width / img_height
+      crop_aspect_ratio = crop_width / crop_height
+
+      crop_rect =
+        if crop_aspect_ratio > desired_aspect_ratio do
+          # The crop is wider than the desired aspect ratio.
+          # That means we are cutting from the sides.
+          height = round(crop_height)
+          width = round(height * desired_aspect_ratio)
+          top = max(0, round(crop_top))
+
+          # Center output horizontally over hotspot
+          hotspot_x_center = round((hotspot_right - hotspot_left) / 2 + hotspot_left)
+          left = max(0, round(hotspot_x_center - width / 2))
+
+          left =
+            cond do
+              left < crop_left ->
+                crop_left
+
+              left + width > crop_left + crop_width ->
+                crop_left + crop_width - width
+
+              true ->
+                left
+            end
+
+          %{
+            left: left,
+            top: top,
+            width: width,
+            height: height
+          }
+        else
+          # The crop is taller than the desired ratio, we are cutting from top and bottom
+          width = crop_width
+          height = round(width / desired_aspect_ratio)
+          left = max(0, round(crop_left))
+
+          # Center output vertically over hotspot
+          hotspot_y_center = round((hotspot_bottom - hotspot_top) / 2 + hotspot_top)
+          top = max(0, round(hotspot_y_center - height / 2))
+
+          # Keep output rect within crop
+          top =
+            cond do
+              top < crop_top ->
+                crop_top
+
+              top + height > crop_top + crop_height ->
+                crop_top + crop_height - height
+
+              true ->
+                top
+            end
+
+          %{
+            left: left,
+            top: top,
+            width: width,
+            height: height
+          }
+        end
+
+      %{
+        width: img_width,
+        height: img_height,
+        rect: crop_rect
+      }
+    end
+  end
+
+  def spec_to_image_url(
+        spec = %{
+          asset: %{
+            id: id,
+            width: width,
+            height: height,
+            format: format
+          }
+        }
+      ) do
+    filename = "#{id}-#{width}x#{height}.#{format}"
+    base_url = "#{file_base()}/images/#{project_id()}/#{dataset()}/#{filename}"
+
+    rect_param =
+      if spec[:rect] do
+        # Only bother url with a crop if it actually crops anything
+        %{left: rect_left, top: rect_top, width: rect_width, height: rect_height} = spec[:rect]
+
+        is_effective_crop =
+          rect_left !== 0 || rect_top !== 0 || rect_height !== height || rect_width !== width
+
+        if is_effective_crop do
+          {"rect", "#{rect_left},#{rect_top},#{rect_width},#{rect_height}"}
+        else
+          nil
+        end
+      else
+        nil
+      end
+
+    focal_point_param =
+      if spec[:focal_point] do
+        [{"fp-x", spec[:focal_point][:x]}, {"fp-y", spec[:focal_point][:y]}]
+      else
+        nil
+      end
+
+    flip_param =
+      if spec[:flip_horizontal] || spec[:flip_vertical] do
+        {"flip",
+         "#{if spec[:flip_horizontal], do: "h", else: ""}#{
+           if spec[:flip_vertical], do: "v", else: ""
+         }"}
+      end
+
+    formatted_params =
+      [
+        rect_param,
+        focal_point_param,
+        flip_param
+      ]
+      |> List.flatten()
+      |> Enum.reject(&is_nil/1)
+
+    # Map from spec name to url param name, and allow using the actual param name as an alternative
+    params =
+      Enum.reduce(@spec_name_to_url_name_mapping, [], fn {transform_key, param_key}, params ->
+        cond do
+          spec[transform_key] -> params ++ [{param_key, spec[transform_key]}]
+          spec[param_key] -> params ++ [{param_key, spec[spec[param_key]]}]
+          true -> params
+        end
+      end)
+
+    (formatted_params ++ params)
+    |> URI.encode_query()
+    |> case do
+      "" -> base_url
+      url_params -> "#{base_url}?#{url_params}"
+    end
   end
 end
--- test/asset_builder_test.exs
+++ test/asset_builder_test.exs
@@ -1,35 +1,182 @@
 defmodule ExSanity.AssetBuilderTest do
   use ExUnit.Case
 
-  describe("file_url/1") do
-    test("handles asset _ref") do
-      ref = "file-88a39460f9a23f524ae80688fff8464b40e4e8ec-mp3"
-      output = ExSanity.AssetBuilder.file_url(ref)
-
-      assert output == "https://cdn.sanity.io/files/123/test/88a39460f9a23f524ae80688fff8464b40e4e8ec.mp3"
-    end
-
-    test("handles asset url") do
-      url = "https://cdn.sanity.io/files/123/test/88a39460f9a23f524ae80688fff8464b40e4e8ec.mp3"
-      output = ExSanity.AssetBuilder.file_url(url)
-
-      assert output == url
-    end
+  def uncropped_image do
+    %{
+      "_type" => "image",
+      "asset" => %{
+        "_ref" => "image-Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000-jpg",
+        "_type" => "reference"
+      },
+      "crop" => %{
+        "bottom" => 0.0,
+        "left" => 0,
+        "right" => 0,
+        "top" => 0
+      },
+      "hotspot" => %{
+        "height" => 0.3,
+        "width" => 0.3,
+        "x" => 0.3,
+        "y" => 0.3
+      }
+    }
   end
 
-  describe("image_url/1") do
-    test("handles asset _ref") do
-      ref = "image-88a39460f9a23f524ae80688fff8464b40e4e8ec-1024x576-jpg"
-      output = ExSanity.AssetBuilder.image_url(ref)
+  def cropped_image do
+    %{
+      "_type" => "image",
+      "asset" => %{
+        "_ref" => "image-Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000-jpg",
+        "_type" => "reference"
+      },
+      "crop" => %{
+        "bottom" => 0.1,
+        "left" => 0.1,
+        "right" => 0.1,
+        "top" => 0.1
+      },
+      "hotspot" => %{
+        "height" => 0.3,
+        "width" => 0.3,
+        "x" => 0.3,
+        "y" => 0.3
+      }
+    }
+  end
 
-      assert output == "https://cdn.sanity.io/images/123/test/88a39460f9a23f524ae80688fff8464b40e4e8ec-1024x576.jpg"
+  # describe("file_url/1") do
+  #   test("handles asset _ref") do
+  #     ref = "file-88a39460f9a23f524ae80688fff8464b40e4e8ec-mp3"
+  #     output = ExSanity.AssetBuilder.file_url(ref)
+
+  #     assert output ==
+  #              "https://cdn.sanity.io/files/123/test/88a39460f9a23f524ae80688fff8464b40e4e8ec.mp3"
+  #   end
+
+  #   test("handles asset url") do
+  #     url = "https://cdn.sanity.io/files/123/test/88a39460f9a23f524ae80688fff8464b40e4e8ec.mp3"
+  #     output = ExSanity.AssetBuilder.file_url(url)
+
+  #     assert output == url
+  #   end
+  # end
+
+  describe("image_url/1") do
+    test "does not crop when no crop is required" do
+      url = ExSanity.AssetBuilder.image_url(%{source: uncropped_image(), transforms: %{}})
+
+      assert URI.decode(url) ==
+               "#{ExSanity.AssetBuilder.file_base()}/images/#{ExSanity.AssetBuilder.project_id()}/#{
+                 ExSanity.AssetBuilder.dataset()
+               }/Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000.jpg"
     end
 
-    test("handles asset url") do
-      url = "https://cdn.sanity.io/images/123/test/88a39460f9a23f524ae80688fff8464b40e4e8ec-1024x576.jpg"
-      output = ExSanity.AssetBuilder.image_url(url)
+    test "does does not crop, but limits size when only width dimension is specified" do
+      url =
+        ExSanity.AssetBuilder.image_url(%{
+          source: uncropped_image(),
+          transforms: %{
+            width: 100
+          }
+        })
 
-      assert output == url
+      assert URI.decode(url) ==
+               "#{ExSanity.AssetBuilder.file_base()}/images/#{ExSanity.AssetBuilder.project_id()}/#{
+                 ExSanity.AssetBuilder.dataset()
+               }/Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000.jpg?w=100"
+    end
+
+    test "does does not crop, but limits size when only height dimension is specified" do
+      url =
+        ExSanity.AssetBuilder.image_url(%{
+          source: uncropped_image(),
+          transforms: %{
+            height: 100
+          }
+        })
+
+      assert URI.decode(url) ==
+               "#{ExSanity.AssetBuilder.file_base()}/images/#{ExSanity.AssetBuilder.project_id()}/#{
+                 ExSanity.AssetBuilder.dataset()
+               }/Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000.jpg?h=100"
+    end
+
+    test "a tall crop is centered on the hotspot" do
+      url =
+        ExSanity.AssetBuilder.image_url(%{
+          source: uncropped_image(),
+          transforms: %{
+            width: 30,
+            height: 100
+          }
+        })
+
+      assert URI.decode(url) ==
+               "#{ExSanity.AssetBuilder.file_base()}/images/#{ExSanity.AssetBuilder.project_id()}/#{
+                 ExSanity.AssetBuilder.dataset()
+               }/Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000.jpg?rect=150,0,900,3000&w=30&h=100"
+    end
+
+    test "a wide crop is centered on the hotspot" do
+      url =
+        ExSanity.AssetBuilder.image_url(%{
+          source: uncropped_image(),
+          transforms: %{
+            width: 100,
+            height: 30
+          }
+        })
+
+      assert URI.decode(url) ==
+               "#{ExSanity.AssetBuilder.file_base()}/images/#{ExSanity.AssetBuilder.project_id()}/#{
+                 ExSanity.AssetBuilder.dataset()
+               }/Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000.jpg?rect=0,600,2000,600&w=100&h=30"
+    end
+
+    test "a crop with identical aspect and no specified crop is not cropped" do
+      url =
+        ExSanity.AssetBuilder.image_url(%{
+          source: uncropped_image(),
+          transforms: %{
+            width: 200,
+            height: 300
+          }
+        })
+
+      assert URI.decode(url) ==
+               "#{ExSanity.AssetBuilder.file_base()}/images/#{ExSanity.AssetBuilder.project_id()}/#{
+                 ExSanity.AssetBuilder.dataset()
+               }/Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000.jpg?w=200&h=300"
+    end
+
+    test "respects the crop, even when no explicit crop is asked for" do
+      url =
+        ExSanity.AssetBuilder.image_url(%{
+          source: cropped_image(),
+          transforms: %{}
+        })
+
+      assert URI.decode(url) ==
+               "#{ExSanity.AssetBuilder.file_base()}/images/#{ExSanity.AssetBuilder.project_id()}/#{
+                 ExSanity.AssetBuilder.dataset()
+               }/Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000.jpg?rect=200,300,1600,2400"
+    end
+
+    test "a tall crop is centered on the hotspot and constrained within the image crop" do
+      url =
+        ExSanity.AssetBuilder.image_url(%{
+          source: cropped_image(),
+          transforms: %{
+            width: 30,
+            height: 100
+          }
+        })
+
+      assert URI.decode(url) ==
+               "#{ExSanity.AssetBuilder.file_base()}/images/#{ExSanity.AssetBuilder.project_id()}/#{
+                 ExSanity.AssetBuilder.dataset()
+               }/Tb9Ew8CXIwaY6R1kjMvI0uRR-2000x3000.jpg?rect=240,300,720,2400&w=30&h=100"
     end
   end
 end
